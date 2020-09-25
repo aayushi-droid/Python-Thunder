b27febb70939bc4ea31202c4fde477a3ff0fb286
Matheus Queiroz <matheusqz@gmail.com>
2020-09-25 16:31:00 -0300

    Add smiley Faces Problem

---
+++ SmileyFaces.py
@@ -0,0 +1,22 @@
+"""
+    Probem Task : Given a string of characters containing only the three characters : ( ) :
+    Create a function returns a number based on the number of sad and smiley faces there are.
+
+    The happy faces :) and (: are worth 1.
+    The sad faces :( and ): are worth -1.
+    Problem Link: https://edabit.com/challenge/8qD23E6XRMaWhyJ5z
+"""
+
+
+def smiley_faces(smile_string):
+    len_string = len(smile_string)
+    score = 0
+    for i in range(1, len_string):
+        smile = smile_string[i - 1] + smile_string[i]
+        print(smile)
+        if smile in [":)", "(:"]:
+            score += 1
+        elif smile in (":(", "):"):
+            score -= 1
+
+    return score
