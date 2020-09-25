d62cdc25a7f96704eda3a62bf6a1b15815672e59
zeenayouhan <zinayouhan33@gmail.com>
2020-09-26 00:45:17 +0530

    listOfMultiplies

---
+++ listOfMultiplies.py
@@ -0,0 +1,5 @@
+def list_of_multiples (num, length):
+	return [i*num for i in range(1,length+1)]
+
+#Problem statement: Create a function that takes two numbers as arguments (num, length) and returns a list of multiples of num up to length.
+#Problem Link: https://edabit.com/challenge/BuwHwPvt92yw574zB
