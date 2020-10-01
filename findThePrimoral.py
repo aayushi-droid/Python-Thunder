a0c3f052d86e00fced02abc257f8cd68409c19b8
Gautam-virmani <52214000+Gautam-virmani@users.noreply.github.com>
2020-10-02 00:04:42 +0530

    Update findThePrimoral.py

--- findThePrimoral.py
+++ findThePrimoral.py
@@ -1,4 +1,7 @@
 #Just enter the number of prime number you want to multiply and you can get the results
+#Problem statement: A Primorial is a product of the first n prime numbers (e.g. 2 x 3 x 5 = 30). 2, 3, 5, 7, 11, 13 are prime numbers. 
+#If n was 3, you'd multiply 2 x 3 x 5 = 30 or Primorial = 30.
+#Problem Link: https://edabit.com/challenge/fRjfrCYXWJAaQqFXF
 
 def find_the_primoral(n):
     s = 2
