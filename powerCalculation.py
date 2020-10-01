9f88384c86890e261265f49401db8127c2f04b67
shubhamgoel9 <72183668+shubhamgoel9@users.noreply.github.com>
2020-10-01 19:00:44 +0530

    Create powerCalculation.py

---
+++ powerCalculation.py
@@ -0,0 +1,18 @@
+'''
+Probem Task : Power Calculator
+    Create a function that takes voltage and current and returns the calculated power.
+    Example:
+    circuit_power(230, 10) ➞ 2300
+
+    circuit_power(110, 3) ➞ 330
+
+    circuit_power(480, 20) ➞ 9600
+    
+    Problem Link: https://edabit.com/challenge/v5gc8FQkDEepkqpfp
+'''
+
+def circuit_power(voltage, current):
+    return voltage*power;
+        
+
+print(circuit_power(230, 10));
