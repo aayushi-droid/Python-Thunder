9fad912e7074f05c073e74d9fa12d39eec30dc24
Rajat Bansal <rajat_mac@rajats-macbook.local>
2020-10-01 19:13:13 +0530

    Added function for factorial

---
+++ factorial.py
@@ -0,0 +1,10 @@
+'''
+    Probem Task : This program will add two numbers
+    Problem Link : https://edabit.com/challenge/PNbsQzmDR3CJ9JHkB
+'''
+import math
+
+def factorial(n):
+    return math.factorial(n)
+
+print (factorial(5))
