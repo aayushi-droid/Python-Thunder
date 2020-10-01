7700c31677ccba4b105aa8ca3064d722941c9391
Pablo Corbalán <pablocorbcon@gmail.com>
2020-10-01 11:54:38 +0200

    minutesToSeconds.py file created
    I have created the file and the function

---
+++ minutesToSeconds.py
@@ -0,0 +1,8 @@
+"""
+This file is the problem related with issue #56
+
+Problem: Write a function that takes an integer minutes and converts it to seconds.
+Problem link: https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+"""
+def min_to_sec(mins):
+  return mins * 60
