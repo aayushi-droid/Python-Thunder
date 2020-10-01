484c0681ff88f2e2afea3e25f3feed849d2b114c
pbisen <65162182+pbisen@users.noreply.github.com>
2020-10-01 19:46:26 +0530

    Create lessThan100.py
    Implemented less than 100
    Problem Link : https://edabit.com/challenge/pZ3HxBfvejsvkEDo4

---
+++ lessThan100.py
@@ -0,0 +1,11 @@
+'''
+    Probem Task : Given two numbers, return True if the sum of both numbers is less than 100. Otherwise return False.
+    Problem Link : https://edabit.com/challenge/pZ3HxBfvejsvkEDo4
+'''
+
+def less_than_100(a, b):
+	if(a + b < 100):
+		return True
+	else:
+		return False
+	
