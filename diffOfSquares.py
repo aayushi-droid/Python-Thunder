a0490e78693477441a309c02e86f05b4cd93ef87
Aditya Punetha <AdityaPunetha6@gmail.com>
2020-10-01 14:02:55 +0530

    Update diffOfSquares.py
    float input is acceptable now

--- diffOfSquares.py
+++ diffOfSquares.py
@@ -7,5 +7,5 @@ Problem Link: https://edabit.com/challenge/NNhkGocuPMcryW7GP
 def square_areas_difference(r):
 		print("The difference in area of incircle square and circumcumcircle square is : " + str(((2*r)**2)-(r**2)*2))
 
-radius = int(input("Enter the radius of the circle: "))
+radius = int(float("Enter the radius of the circle: "))
 square_areas_difference(radius)
