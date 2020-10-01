0a2ee107838fbbdb2e0ee43f33cc56ca918885af
A V S ADITYAVARDHAN <47914143+AVS18@users.noreply.github.com>
2020-10-01 15:30:01 +0530

    return Area of Square
    Use formula 2*r*2 => 4r*2 - 2r*2

---
+++ returnSquareArea.py
@@ -0,0 +1,7 @@
+'''
+Problem statement: We need to find the area difference between the squares where for one square circle is a circumcircle and the other one is incircle
+Problem Link: https://edabit.com/challenge/NNhkGocuPMcryW7GP
+'''
+
+def square_areas_difference(r):
+	return 2*r*r
