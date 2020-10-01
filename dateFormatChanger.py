83a4e549fb2c86c7fd5c236d944a35126c658718
iriyagupta <riyagupta771995@gmail.com>
2020-10-02 03:06:29 +0530

    made changes

--- dateFormatChanger.py
+++ dateFormatChanger.py
@@ -1,4 +1,18 @@
+#Before jumping into Pr first comment for assign.All Problem from Edabit. Link is mandatory to add
+
+#Problem statement: Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
+
+#Examples:
+
+#format_date("11/12/2019") ➞ "20191211"
+
+#format_date("12/31/2019") ➞ "20193112"
+
+#format_date("01/15/2019") ➞ "20191501"
+
 #easy method 1
+
+
 
 import datetime
 import argparse
