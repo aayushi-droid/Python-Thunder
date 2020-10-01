8433474970fc1e43cc93b8d64f34318f757ec233
Akansha Jain <jakansha2001@gmail.com>
2020-10-01 11:58:18 +0530

    Adding returnTheSumOfTwoNumbers file

---
+++ returnTheSumOfTwoNumbers.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+      return a+b
+	
+	
+#num1 = input(int("Enter first number - ")
+#num2 = input(int("Enter second number - ")
+
+print("addition(2,3)",addition(2,3))
+print("addition(-3,-6)",addition(-3,-6))
+print("addition(7,3)",addition(7,3))
