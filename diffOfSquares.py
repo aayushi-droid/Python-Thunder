4490e101b021e93e04790181a5de7de27c12470d
Aditya Punetha <AdityaPunetha6@gmail.com>
2020-10-01 13:54:02 +0530

    Update diffOfSquares.py
    Made it user friendly

--- diffOfSquares.py
+++ diffOfSquares.py
@@ -3,5 +3,9 @@ Probem Task : This program returns the difference in areas of two squares.
 Problem Link: https://edabit.com/challenge/NNhkGocuPMcryW7GP
 '''
 
+
 def square_areas_difference(r):
-		return (2*r)**2-(r**2)*2
+		print("The difference in area of incircle square and circumcumcircle square is : " + str(((2*r)**2)-(r**2)*2))
+
+radius = int(input("Enter the radius of the circle: "))
+square_areas_difference(radius)
