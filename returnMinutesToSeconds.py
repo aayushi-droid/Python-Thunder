4b9b56315fd142e13f397751c503f9d15d554e70
ShravyaMallya <mallyashravya@gmail.com>
2020-10-01 15:46:02 +0530

    Commit1

---
+++ returnMinutesToSeconds.py
@@ -0,0 +1,8 @@
+-----
+
+Problem statement: Write a function that takes an integer minutes and converts it to seconds.
+Problem Link: https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+-------
+def convert(minutes):
+	return minutes*60;
