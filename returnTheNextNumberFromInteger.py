c5453dd6741c4a695669a4b21bc3b16d5830d872
ronanren <renouxpro@gmail.com>
2020-10-01 16:15:25 +0200

    Add function theNextNumberFromInteger

---
+++ returnTheNextNumberFromInteger.py
@@ -0,0 +1,9 @@
+'''
+    Probem Task : This function takes a number as an argument, increments the number by +1 and returns the result.
+    Problem Link : https://edabit.com/challenge/KjCS7occ9hfu5snpb
+'''
+
+def addition(num):
+    return num + 1
+
+print(addition(9))
