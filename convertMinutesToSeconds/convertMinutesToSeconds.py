fe74b6cbfb87c40a944d2364d429b8a8cdf87e12
azizamukhamedova <66719879+azizamukhamedova@users.noreply.github.com>
2020-10-01 15:03:04 +0300

    convertMinutesToSeconds added by azizamukhamedova

---
+++ convertMinutesToSeconds/convertMinutesToSeconds.py
@@ -0,0 +1,7 @@
+#This program will convert minutes to seconds
+#Problem Link: https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+minutes = int(input('Minutes:'))
+seconds = minutes * 60
+
+print(f'{seconds} seconds')
