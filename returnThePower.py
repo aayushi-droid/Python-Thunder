2b13dfb1e6a2006dfa0c602e494506ae099e3d84
Harsh Udai <harshudai2001@gmail.com>
2020-10-01 19:16:56 +0530

    Create returnThePower.py
    Added the circuit power function.
    Learning the open source contribution

---
+++ returnThePower.py
@@ -0,0 +1,13 @@
+'''
+    Probem Task : Power Calculation
+    Problem Link : https://edabit.com/challenge/v5gc8FQkDEepkqpfp
+'''
+
+def circuitPower(voltage,current):
+    return voltage*current
+
+#Example:
+
+circuitPower(230,10) # answer 2300 (circuit power function just multiply the voltage and current)
+
+circuitPower(110,3) # answer 330
