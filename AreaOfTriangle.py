11b8d1a23415663de10457d122d43d8adbf95172
st1891 <72141315+st1891@users.noreply.github.com>
2020-10-02 13:05:56 +0530

    Update AreaOfTriangle.py

--- AreaOfTriangle.py
+++ AreaOfTriangle.py
@@ -2,8 +2,17 @@
     Probem Task : Write a function that takes the base and height of a triangle and returns its area.
     Problem Link : https://edabit.com/challenge/3CaszbdZYGN4otQD8
 '''
+
+# The main function
 def tri_area(base, height):
 	return (base*height)/2
   
 base,height=[int(i) for i in input("Enter base and height: ").split()]
 print(tri_area(base,height))
+
+
+
+
+
+
+
