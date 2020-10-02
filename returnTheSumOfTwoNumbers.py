00b68c91605601cadea974d1c7806543a6dbf320
Akansha Jain <jakansha2001@gmail.com>
2020-10-02 10:14:41 +0530

    Return The Sum Of Two Numbers  (#48)
    * Adding returnTheSumOfTwoNumbers file
    
    * Added problem statement and problem link in file
    
    * Adding factorialOfANumber file
    
    * Deleted factorialOfANumebr file

---
+++ returnTheSumOfTwoNumbers.py
@@ -0,0 +1,15 @@
+#Problem statement: Create a function that takes two numbers as arguments and return their sum.
+#Problem Link: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
+
+
+
+def addition(a, b):
+      return a+b
+	
+	
+#num1 = input(int("Enter first number - ")
+#num2 = input(int("Enter second number - ")
+
+print("addition(2,3)",addition(2,3))
+print("addition(-3,-6)",addition(-3,-6))
+print("addition(7,3)",addition(7,3))
