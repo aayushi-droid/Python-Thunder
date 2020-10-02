856302b9d5c3b15512a6ddb80f274a150e5e080f
SUKANTHEN.S.S <49028941+Sukanthen@users.noreply.github.com>
2020-10-02 13:24:20 +0530

    Is the Number Less than or Equal to Zero
    This Python Program takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.

---
+++ IsTheNumberGreaterorLesserthanZero.py
@@ -0,0 +1,8 @@
+'''
+    Probem Task : This Program takes a number as its only argument and returns True if it's less than or equal to zero, otherwise return False.
+    Problem Link : https://edabit.com/challenge/Rx2pkSA9dCmtwS8xt
+'''
+def isnumless(num):
+    if num <= 0:
+        return True
+print(isnumless(2))
