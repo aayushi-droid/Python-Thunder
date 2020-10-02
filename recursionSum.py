d0bc5b0178fc3bedd6f39522796a442a6720c1d7
Neel Gorasiya <mgorasiya1974@gmail.com>
2020-10-02 09:47:17 +0530

    created recursionSum.py

---
+++ recursionSum.py
@@ -0,0 +1,12 @@
+'''
+    Probem Task : Write a function that finds the sum of the first n natural numbers. Make your function recursive.
+    Problem Link : https://edabit.com/challenge/si2H6WC5YX99cn6LQ
+'''
+
+def sum_numbers(x):
+    if x != 0:
+        x += sum_numbers(x-1)
+    else:
+        return 0
+    return x
+
