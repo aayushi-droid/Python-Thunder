70be6b3de08d1bb7def72cebc2f7439368f57eae
SUKANTHEN.S.S <49028941+Sukanthen@users.noreply.github.com>
2020-10-02 13:25:14 +0530

    Delete IsTheNumberGreaterorLesserthanZero.py

--- IsTheNumberGreaterorLesserthanZero.py
+++
@@ -1,8 +0,0 @@
-'''
-    Probem Task : This Program takes a number as its only argument and returns True if it's less than or equal to zero, otherwise return False.
-    Problem Link : https://edabit.com/challenge/Rx2pkSA9dCmtwS8xt
-'''
-def isnumless(num):
-    if num <= 0:
-        return True
-print(isnumless(2))
