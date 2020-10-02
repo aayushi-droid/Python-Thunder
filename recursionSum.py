0b2fef448b11eae622e10ec2d225cb9f363e215c
st1891 <72141315+st1891@users.noreply.github.com>
2020-10-02 13:08:12 +0530

    Update recursionSum.py

--- recursionSum.py
+++ recursionSum.py
@@ -2,7 +2,7 @@
     Probem Task : Write a function that finds the sum of the first n natural numbers. Make your function recursive.
     Problem Link : https://edabit.com/challenge/si2H6WC5YX99cn6LQ
 '''
-
+# Create the function 
 def sum_numbers(x):
     if x != 0:
         x += sum_numbers(x-1)
