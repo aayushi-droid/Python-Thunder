9489e054e0fa8d1fbf231a465525ac364cda87fb
bEERy <siradanai27@gmail.com>
2020-10-02 14:44:37 +0700

    Add remainder program

--- remainder.py
+++ remainder.py
@@ -1,7 +1,8 @@
 '''
-    Probem Task : Create a function that takes an array and finds the integer which appears an odd number of times.
-    Problem Link : https://edabit.com/challenge/wgnmQTbfssuhvZHqe
+    Probem Task : There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+    Problem Link : https://edabit.com/challenge/KWoj7kWiHRqJtG6S2
 '''
+
 
 def remainder(x, y):
     return x % y
