214a34f8833fa62b0b7415ce3469c99a24353658
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2020-10-02 06:56:16 +0100

    Updated the power calculator

--- Solutions/PowerCalculator.py
+++ Solutions/PowerCalculator.py
@@ -1,3 +1,6 @@
+#problem statement:Power Calculator
+#problem link:https://github.com/Py-Droid/Python-Thunder/issues/113
+#link:https://edabit.com/challenge/v5gc8FQkDEepkqpfp
 # this code Creates a function that takes voltage and current and returns the calculated power.
 
 def circuit_power(vol, current):
