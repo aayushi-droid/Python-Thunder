9cd294c890681bb01185dcdee1bd5aa9a16e807f
Shivang-Agarwal11 <65328598+Shivang-Agarwal11@users.noreply.github.com>
2020-10-02 13:01:22 +0530

    Add add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+'''
+    Probem Task : This program will add two numbers
+    Problem Link : https://edabit.com/challenge/SFzHtm63XT6EYNHWY
+'''
+def addition(a,b):
+    return a+b
+a=int(input('Enter number one\n'))
+b=int(input('Enter number two\n'))
+print(addition(a,b))
