986df5cff8d045712dc9e92b72c8981d919005be
bEERy <siradanai27@gmail.com>
2020-10-02 14:37:50 +0700

    Add remainder program

---
+++ remainder.py
@@ -0,0 +1,8 @@
+'''
+    Probem Task : Create a function that takes an array and finds the integer which appears an odd number of times.
+    Problem Link : https://edabit.com/challenge/wgnmQTbfssuhvZHqe
+'''
+
+
+def remainder(x, y):
+    return x % y
