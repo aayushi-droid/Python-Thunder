b8424ef9c26e7fe96fdce3b785ca6147530aad36
Andras Marton <andras.marton@wpengine.com>
2020-10-02 00:02:06 -0500

    Burrrrp (#125)
    * Initial commit of longBurp.py for Problem Link: https://edabit.com/challenge/irb783PpFTDqnumhN
    
    * Initial commit of longBurp.py for Burrrrrrrp #119

---
+++ longBurp.py
@@ -0,0 +1,23 @@
+"""
+    Problem statement: Create a function that returns the string 'Burp' with the amount of 'r's' determined by the input parameters of the function.
+    Problem Link: https://edabit.com/challenge/irb783PpFTDqnumhN
+"""
+
+import sys
+
+def long_burp(num):
+    try:
+        num = int(num)
+        if num >= 1:
+            r = "r" * num
+            return r
+        else:
+            print("The number must be equal to or greater than 1")
+            sys.exit()
+    except ValueError:
+        print("Input must be an integer")
+        sys.exit()
+
+num = input("Provide a number: ")
+r = long_burp(num)
+print("Bu" + r + "p")
