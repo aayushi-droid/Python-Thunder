3a8a75d27ff38e84a8a294deda96252c06a97683
Sarsiz Chauhan <sarsiz97@gmail.com>
2020-10-02 11:01:02 +0530

    Added conversion of hour to seconds in Python

---
+++ Solutions/hourToSeconds.py
@@ -0,0 +1,9 @@
+"""
+    Problem statement: Convert Hours into Seconds
+    Problem Link: https://edabit.com/challenge/nyeNvKWdDFKRAk4Da
+"""
+
+def how_many_seconds(hours):
+    hour_to_seconds = 3600
+	return hours * hour_to_seconds
+
