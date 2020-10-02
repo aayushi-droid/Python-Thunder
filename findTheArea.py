6ee87d86a93d7a24e10f3b6def5b3786a7e3464e
Neel Gorasiya <mgorasiya1974@gmail.com>
2020-10-02 10:30:58 +0530

    created findTheArea.py (#127)

---
+++ findTheArea.py
@@ -0,0 +1,10 @@
+'''
+
+Problem statement: Write a function that takes the base and height of a triangle and return its area.
+Problem Link: https://edabit.com/challenge/3CaszbdZYGN4otQD8
+
+'''
+
+def area(height, width):
+    return 0.5*height*width
+
