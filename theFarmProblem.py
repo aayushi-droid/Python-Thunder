f87eec1825b78822bef1ff37d49d892a16c7cd0f
Albert <35526099+Albertosaurusrex@users.noreply.github.com>
2020-10-02 09:34:59 +0200

    Create theFarmProblem.py
    Solving the problem outlined at https://edabit.com/challenge/QzXtDnSZL6y4ZcEvT
    
    This is solving issue Py-Droid#70 by using a simple function.
    
    ## Checklist
    [☑] Yes, this task assigned to 
    [☑] My code follows the style guidelines(Clean Code) of this 
    [☑] I have performed a self-review of my own 
    [☑] My Filename is in camelCase 
    [☑] I have added Problem statement, Problem link in file.

---
+++ theFarmProblem.py
@@ -0,0 +1,26 @@
+# In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals
+# https://edabit.com/challenge/QzXtDnSZL6y4ZcEvT
+
+
+def legs(chickens, cows, pigs):
+    # adding the amount of chicken legs to a new variable for the total amount of legs.
+    totalLegs = (chickens*2)
+
+    # doing the same for cows 
+    totalLegs = totalLegs + (cows*4)
+
+    # lastly i do it for pigs.
+    totalLegs = totalLegs + (pigs*4)
+
+    return(totalLegs)
+
+print("Input amount of Chickens")
+x = int(input())
+
+print("Input amount of Cows")
+y = int(input())
+
+print("Input amount of Pigs")
+z = int(input())
+
+print(legs(x, y, z))
