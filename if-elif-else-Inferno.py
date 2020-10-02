75561d79434e5a3379d75c4d41cb64fb8f9e8b40
Amit Kumar Agarwal <amitagarwal.aa57@gmail.com>
2020-10-02 10:50:39 +0530

    Update if-elif-else-Inferno.py

--- if-elif-else-Inferno.py
+++ if-elif-else-Inferno.py
@@ -1,2 +1,6 @@
+'''
+Problem Statement : Write a function that returns the boolean True if the given number is zero, the string "positive" if the number is greater than zero or the string "negative" if it's smaller than zero.
+Problem Link : https://edabit.com/challenge/2TdPmSpLpa8NWh6m9
+'''
 def equilibrium(x):
 	return not x or ('negative', 'positive')[x > 0]
