f90969f6bcf1ccccbad97e8ac2bd6db35b26b3a2
189302081 <sanjilmahajani@gmail.com>
2020-10-02 15:06:34 +0530

    Added a code to convert minutes to secs

---
+++ mintosec.py
@@ -0,0 +1,7 @@
+#Problem statement-Write a function that takes an integer minutes and converts it to seconds.
+#Problem link-https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+def minToSecs(min):
+    min=int(input('Enter the minutes'))
+    sec=min*60
+    return sec
