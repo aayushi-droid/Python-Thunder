c6ecb167ab4afbce0f0edb7862bed3856427a11f
Rookie3109 <65118866+Rookie3109@users.noreply.github.com>
2020-10-03 09:26:32 +0400

    Create NumbersToEnglish.py

---
+++ Solutions/NumbersToEnglish.py
@@ -0,0 +1,36 @@
+H100 = "Hundred "
+
+
+def convert2digits(n: int) -> str:
+    upto20 = ["", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ",
+              "Eight ", "Nine ", "Ten ", "Eleven ", "Twelve ", "Thirteen ",
+              "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "]
+    tens = ["", "", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ",
+            "Eighty ", "Ninety "]
+
+    if n < len(upto20):
+        return upto20[n]
+    return tens[n // 10] + upto20[n % 10]
+
+
+def convert3digits(n: int) -> str:
+    if n < 100:
+        return convert2digits(n)
+    return fix_and(convert2digits(n // 100) + H100 + convert2digits(n % 100))
+
+
+def fix_and(s: str) -> str:
+    if H100 is s and not s.endswith(H100):
+        return s.replace(H100, H100 + "and ")
+    else:
+        return s
+
+
+def NumbersToEnglish(n: int) -> str:
+    if n == 0:
+        return "Zero"
+    return convert3digits(n).capitalize()
+
+
+n = int(input())
+print(NumbersToEnglish(n))
