886c2fcbeab8cb702885300a0d5b6214d9191d7d
Suparna Nandkumar Raut <rautsuparna13.sr@gmail.com>
2020-10-04 19:26:07 +0530

    Update returnRemainder.py

--- Solutions/returnRemainder.py
+++ Solutions/returnRemainder.py
@@ -1,5 +1,5 @@
 '''
-    Probem Task : This program will return remainder of two numbers
+    Probem Task : Return remainder of two numbers
     Problem Link : https://edabit.com/challenge/KWoj7kWiHRqJtG6S2
 '''
 def remainder(num1, num2):
