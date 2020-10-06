2fea28b594f87e6996837379988783fe3eec7547
Anirudha gudi <54411756+gudianirudha@users.noreply.github.com>
2020-10-06 21:13:05 +0530

    Create Convert_Min_To_Seconds.py

---
+++ Convert_Min_To_Seconds.py
@@ -0,0 +1,11 @@
+'''
+
+Problem statement: Write a function that takes an integer minutes and converts it to seconds.
+Problem Link : https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+'''
+
+def convert(minutes):
+	return 60*minutes
+
+	
