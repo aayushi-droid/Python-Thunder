30f0874727911f2b5c09f78c1714bda55469027f
Chidambar Joshi <54184017+Chidu2000@users.noreply.github.com>
2020-10-06 20:42:56 +0530

    Create ConvertMinutesintoSeconds.py
    python code for converting minutes to equivalent seconds

---
+++ ConvertMinutesintoSeconds.py
@@ -0,0 +1,10 @@
+''' problem statement: Write a program to convert minutes into seconds
+    
+    problem link: https://edabit.com/challenge/FQyaaJx7orS7tiwz8     '''
+    
+    
+    def convert(minutes):
+	      seconds=minutes*60
+	      return seconds
+	
+    convert(10)
