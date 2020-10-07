2b5858ed90df2b3e47f6f41010dc5b8d8a4043b2
Yosua Ricky Tjipta <66916789+ysricky@users.noreply.github.com>
2020-10-07 16:38:59 +0700

    added divisibleByFive.py #164 (#172)
    * added DivisibleByFive.py #164
    
    * Rename DivisibleByFive.py to divisibleByFive.py

---
+++ divisibleByFive.py
@@ -0,0 +1,8 @@
+'''
+    Probem Task : Create a function that returns True if an integer is evenly divisible by 5, and False otherwise.
+    Problem Link : https://edabit.com/challenge/49pyDP8dE3pJ2dYMW
+'''
+def divisible_by_five(n):
+	return n%5 == 0
+
+divisible_by_five(125)
