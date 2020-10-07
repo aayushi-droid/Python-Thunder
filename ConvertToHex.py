0a79a88bbc8b12fb5dad202732fd5d86b680b26a
Vempalli Mugenna Gari Madhava Reddy <38190683+vmgmreddy@users.noreply.github.com>
2020-10-07 14:35:37 +0530

    Update ConvertToHex.py

--- ConvertToHex.py
+++ ConvertToHex.py
@@ -1,3 +1,8 @@
+'''  
+  Problem statement: Create a function that takes a strings characters as ASCII and returns each characters hexadecimal value as a string.
+  Problem Link: https://edabit.com/challenge/g6yjSfTpDkX2STnSX
+'''
+
 def convert_to_hex(txt):
   solution=""
   for chr in txt:
