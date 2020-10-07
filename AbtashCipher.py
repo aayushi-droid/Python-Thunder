71c924dfcdb94233430f1f156d756d97679cc44e
Kuruvilla Jacob <36655830+KuruvillaJacob02@users.noreply.github.com>
2020-10-07 13:09:08 +0300

    Update AbtashCipher.py

--- AbtashCipher.py
+++ AbtashCipher.py
@@ -1,4 +1,9 @@
 #use _author_ = 'Kuruvilla Jacob'
+'''
+    Probem Task:  The Atbash cipher is an encryption method in which each letter of a word is replaced with its "mirror" letter in the alphabet: A <=> Z; B <=> Y; C <=> X; etc.
+    Create a function that takes a string and applies the Atbash cipher to it.
+    Problem Link: https://edabit.com/challenge/MGALfBAXhXqqdFyqo
+'''
 def atbash(string):
     s2=''
     for char in string:
