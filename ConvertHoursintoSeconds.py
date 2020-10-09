0f4ce83c276fb8fd6507a37a162ffb04ee1db158
V Roshan Kumar Patro <varanasiroshan31011@gmail.com>
2020-10-09 16:35:37 +0530

    Create ConvertHoursintoSeconds.py

---
+++ ConvertHoursintoSeconds.py
@@ -0,0 +1,12 @@
+'''
+    Probem Task : Create a function that returns True if an integer is evenly divisible by 5, and False otherwise.
+    Problem Link : https://edabit.com/challenge/nyeNvKWdDFKRAk4Da
+'''
+
+def how_many_seconds(hours):
+	hours = hours * 3600
+	return hours
+  
+  
+ how_many_seconds(200)
+ 
