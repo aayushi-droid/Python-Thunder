e62611abe8e1c6c1983801b10edb1c547d5c2a1f
v-j-0 <72620627+v-j-0@users.noreply.github.com>
2020-10-13 01:28:58 +0530

    Added calcAge (#303)
    Co-authored-by: alien hawk <thealienhawk@gmail.com>

---
+++ calcAge.py
@@ -0,0 +1,10 @@
+'''
+
+Problem statement: Create a function that takes the age and return the age in days.
+Problem Link: https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw
+
+'''
+
+
+def calcAge(age):
+    return age * 365
