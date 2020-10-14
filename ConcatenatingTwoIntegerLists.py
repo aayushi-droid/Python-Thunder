953a421033db60f076ae256893dc1a7f3eeae8ad
nutankushwah <35323118+nutankushwah@users.noreply.github.com>
2020-10-14 07:49:48 +0530

    Add solution to Concatenating Two Integer Lists

---
+++ ConcatenatingTwoIntegerLists.py
@@ -0,0 +1,7 @@
+'''
+    Problem Task : Create a function to concatenate two integer lists.
+    Problem Link : https://edabit.com/challenge/cCWMeiJCP9Ef8XMq8
+'''
+
+def concat(lst1, lst2):
+  return lst1 + lst2
