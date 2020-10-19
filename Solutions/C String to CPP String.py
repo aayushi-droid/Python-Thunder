5a34240f61f169fe1084a46a027705b5cc631db6
Ares358 <52101053+Ares358@users.noreply.github.com>
2020-10-19 17:16:57 +0530

    added problem statement and problem link

--- Solutions/C String to CPP String.py
+++ Solutions/C String to CPP String.py
@@ -1,3 +1,12 @@
+
+'''
+Problem statement: This is a list of single characters with an unwanted character at the end:
+["H", "e", "l", "l", "o", "!", "\0"]
+You could also just type "Hello!" when initializing a variable, creating the string "Hello!"
+Create a function that will return a string by combining the given character list, not including the unwanted final character.
+
+Problem Link: https://edabit.com/challenge/F4iemEeFfsaFoMpAF
+'''
 def cpp_txt(lst):
 	ans=""
 	for i in range(0,len(lst)-1):
