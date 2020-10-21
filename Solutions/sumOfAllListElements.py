5e1da5ecfd9366a8c55e8858a240e18b536fed45
N S S KOUSHIIK <40133414+nssk1999@users.noreply.github.com>
2020-10-21 16:47:38 +0530

    added sumOfAllListElements.py for #347
    added sumOfAllListElements.py to solve the issue  #347 raised my me

---
+++ Solutions/sumOfAllListElements.py
@@ -0,0 +1,10 @@
+'''
+Problem Statement: Create a function that takes a list and returns the sum of all numbers in the list.
+Problem Link: https://edabit.com/challenge/foFKdr68vSENQ9AYB
+'''
+
+def get_sum_of_elements(arr):
+	return sum(arr)
+
+print(get_sum_of_elements([2, 7, 4]))
+#get_sum_of_elements([2, 7, 4]) ➞ 13
