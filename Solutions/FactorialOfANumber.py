76d07e17162c929b89dd10ddac43400895447099
sahilsingh2402 <44718639+sahilsingh2402@users.noreply.github.com>
2020-10-26 09:26:24 +0530

    Added Recursion.py

--- Solutions/FactorialOfANumber.py
+++ Solutions/FactorialOfANumber.py
@@ -1,17 +1,18 @@
-'''
-Problem Statement: Create a function that receives a non-negative
-integer and returns the factorial of that number.
+# Problem statement: Create a function that receives a non-negative integer and returns the factorial of that number.
+# Problem Link: https://edabit.com/challenge/PNbsQzmDR3CJ9JHkB
 
-Problem Link: https://edabit.com/challenge/PNbsQzmDR3CJ9JHkB
-'''
 
 def fact(n):
-	if n<0:
-		print("you have entered invalid no. please retry")
-	if n == 0:
-		return 1
-	else:
-		output = 1
-		for i in range(1, n+1):
-			output *= i
-		return output
+    fct = 1
+    for i in range(1, n + 1):
+        fct = fct * i
+    return fct
+
+
+print("fact(0)", fact(0))
+print("fact(1)", fact(1))
+print("fact(2)", fact(2))
+print("fact(3)", fact(3))
+print("fact(7)", fact(7))
+print("fact(9)", fact(9))
+print("fact(15)", fact(15))
